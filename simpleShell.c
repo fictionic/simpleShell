a94f895c8800291ff31a0974047d666d344923b7
seeegma <dylandforbes@gmail.com>
2016-04-11 16:41:27 -0500

    it works, I think

--- simpleShell.c
+++ simpleShell.c
@@ -16,11 +16,14 @@ size_t MAX_NUM_TOKENS = 51;
 char* readLine();
 char** lex(char* line);
 char*** parse(char* line);
+void print_strings(char** strings);
+int spawn_process(int fd_in, int fd_out, char** cmd);
 
 int main() {
 	while(1) {
 		fflush(stdout);
 		printf("simpleShell $ ");
+		fflush(stdout);
 
 		// read input into string
 		char* line = readLine();
@@ -45,10 +48,11 @@ int main() {
 			}
 			i++;
 		}
-		
+
 		// make sure the first token isn't a pipe
 		if(tokens[0][0] == '|' && tokens[0][1] == '\0') {
 			printf("Error: can't have pipe be first token\n");
+			fflush(stdout);
 			return 1;
 		}
 
@@ -63,10 +67,9 @@ int main() {
 			no_wait = 0;
 
 		// assemble tokens into array of commands
-		// first, allocate arrays for commands, their lengths, and the pipes that will connect them
+		// first, allocate arrays for commands and their lengths
 		char*** commands = malloc(num_commands * sizeof(char**));
-		int* command_lengths = malloc(num_commands * sizeof(int));
-		int** pipes = malloc((num_commands-1) * sizeof(int*));
+		int* tokens_in_command = malloc(num_commands * sizeof(int));
 
 		i = 1;
 		int first_token_of_last_cmd = 0;
@@ -76,32 +79,30 @@ int main() {
 			// check if the current token is a pipe symbol
 			if(tokens[i][0] == '|' && tokens[i][1] == '\0')
 				is_pipe = 1;
-			 else
+			else
 				is_pipe = 0;
 			// make sure we don't have two pipes in a row
 			if(is_pipe && i == first_token_of_last_cmd) {
 				printf("Error: can't have two pipes in a row\n");
+				fflush(stdout);
 				return 1;
 			}
 			if(is_pipe) {
 				// replace the pipe token from the array with a null pointer
 				free(tokens[i]);
 				tokens[i] = NULL;
-				// create pipes
-				pipes[cmd_i] = malloc(2);
 				// store sequence of tokens leading up to the pipe into the commands array
 				commands[cmd_i]= &tokens[first_token_of_last_cmd];
-				command_lengths[cmd_i] = i - first_token_of_last_cmd;
+				tokens_in_command[cmd_i] = i - first_token_of_last_cmd;
 				// prepare to do the same for the next command
 				cmd_i++;
 				first_token_of_last_cmd = i+1;
-				pipe(pipes[cmd_i]);
 			}
 			i++;
 		}
 		// assemble final command
 		commands[cmd_i]= &tokens[first_token_of_last_cmd];
-		command_lengths[cmd_i] = i - first_token_of_last_cmd;
+		tokens_in_command[cmd_i] = i - first_token_of_last_cmd;
 
 		// -- iterate through commands -- //
 		// allocate arrays for strings of files for io redirection
@@ -110,110 +111,165 @@ int main() {
 		// fill the arrays, and remove the redirection tokens from the command token arrays
 		for(cmd_i = 0; cmd_i < num_commands; cmd_i++) {
 			// look for file redirection operators
-			//char** pure_command = malloc((command_lengths[cmd_i] + 1) * sizeof(char*));
+			//char** pure_command = malloc((tokens_in_command[cmd_i] + 1) * sizeof(char*));
 			int pure_command_token_i = 0;
 			infiles[cmd_i] = NULL;
 			outfiles[cmd_i] = NULL;
 			// iterate through tokens within the current command
 			i = 0;
-			for(; i < command_lengths[cmd_i]; i++) {
+			while(i < tokens_in_command[cmd_i]) {
 				// check for input redirection
 				if(commands[cmd_i][i][0] == '<' && commands[cmd_i][i][1] == '\0') {
 					// make sure there's a token after this
-					if(commands[cmd_i][i+1] == NULL || i-1 == command_lengths[cmd_i]) {
+					if(commands[cmd_i][i+1] == NULL || i-1 == tokens_in_command[cmd_i]) {
 						printf("Error: need to specify file for input redirection\n");
+						fflush(stdout);
 						return 1;
 					} else {
-						if(infiles[cmd_i] != NULL)
+						if(infiles[cmd_i] != NULL) {
 							printf("Warning: ignoring additional stdin redirect operator\n");
+							fflush(stdout);
+						}
 						else
 							infiles[cmd_i] = commands[cmd_i][i+1];
 						// skip the token that gives the filename
 						i++;
 					}
 				} else
-				// check for output redirection
-				if(commands[cmd_i][i][0] == '>' && commands[cmd_i][i][1] == '\0') {
-					// make sure there's a token after this
-					if(commands[cmd_i][i+1] == NULL || i-1 == command_lengths[cmd_i]) {
-						printf("Error: need to specify file for output redirection\n");
-						return 1;
+					// check for output redirection
+					if(commands[cmd_i][i][0] == '>' && commands[cmd_i][i][1] == '\0') {
+						// make sure there's a token after this
+						if(commands[cmd_i][i+1] == NULL || i-1 == tokens_in_command[cmd_i]) {
+							printf("Error: need to specify file for output redirection\n");
+							fflush(stdout);
+							return 1;
+						} else {
+							if(outfiles[cmd_i] != NULL) {
+								printf("Warning: ignoring additional stdout redirect operator\n");
+								fflush(stdout);
+							}
+							else
+								outfiles[cmd_i] = commands[cmd_i][i+1];
+							// skip the token that gives the filename
+							i++;
+						}
 					} else {
-						if(outfiles[cmd_i] != NULL)
-							printf("Warning: ignoring additional stdout redirect operator\n");
-						else
-							outfiles[cmd_i] = commands[cmd_i][i+1];
-						// skip the token that gives the filename
-						i++;
+						// add next non-redirect token to pure_command
+						commands[cmd_i][pure_command_token_i] = commands[cmd_i][i];
+						pure_command_token_i++;
 					}
-				} else {
-					// add next non-redirect token to pure_command
-					commands[pure_command_token_i] = commands[cmd_i];
-					pure_command_token_i++;
-				}
-				//commands[cmd_i] = pure_command;
+				i++;
 			}
 			// end command here
-			commands[pure_command_token_i] = NULL;
+			commands[cmd_i][pure_command_token_i] = NULL;
 		}
 
-		// fork and execute each file
-		for(cmd_i = 0; cmd_i < num_commands; cmd_i++) {
-			// fork
-			int childPID = fork();
-			if(childPID == 0) { // child process
-				// redirect stdin to file, if necessary
-				if(infiles[cmd_i] != NULL) {
-					// make sure we're the first command in the pipeline
-					if(cmd_i > 0) {
-						printf("Error: can't have input redirection after first command in pipeline\n");
-						return 1;
-					}
-					int in_fd = open(infiles[cmd_i], O_RDONLY);
-					if(dup2(in_fd, 0) == -1)
-						printf("Error 1: %s\n", strerror(errno));
-				} else {
-					if(pipes[cmd_i] != NULL) {
-						dup2(pipes[cmd_i][0], 0);
-						close(pipes[cmd_i][1]);
-					}
-				}
+		// execute each process in the pipeline with appropriate file descriptors
+		pid_t childPID;
+		int fd_in, fd_out;
+		int cur_pipe[2];
 
-				// redirect stdout to file, if necessary
-				if(outfiles[cmd_i] != NULL) {
-					// make sure we're the last command in the pipeline
-					if(cmd_i < num_commands - 1) {
-						printf("Error: can't have output redirection before last command in pipeline\n");
-						return 1;
-					}
-					int out_fd = open(outfiles[cmd_i], O_WRONLY | O_CREAT);
-					if(dup2(out_fd, 1) == -1)
-						printf("Error 2: %s\n", strerror(errno));
-				} else {
-					if(pipes[cmd_i] != NULL) {
-						dup2(pipes[cmd_i][1], 1);
-						close(pipes[cmd_i][0]);
-					}
-				}
-
-				// execute program
-				if(execvp(commands[cmd_i][0], commands[cmd_i]) == -1) {
-					printf("Error 3: %s\n", strerror(errno));
-					return 1;
-				}
-			} else { // parent process
-				// wait for child to terminate, if we're supposed to
-				if(!no_wait)
-					waitpid(childPID, NULL, 0);
+		// redirect stdin to file, if necessary
+		if(infiles[0] != NULL) {
+			// try to open the input file
+			fd_in = open(infiles[0], O_RDONLY);
+			if(fd_in == -1) {
+				printf("open: Error: %s\n", strerror(errno));
+				fflush(stdout);
+				return 1;
 			}
+		} else {
+			fd_in = 0; // first child's input is parent's input by default
 		}
+
+		for(cmd_i = 0; cmd_i < num_commands-1; cmd_i++) {
+			
+			// create pipe
+			pipe(cur_pipe);
+			//printf("cur_pipe: [%d,%d]\n", cur_pipe[0], cur_pipe[1]);
+
+			// send output to the next pipe
+			fd_out = cur_pipe[1];
+
+			// fork and exec
+			childPID = spawn_process(fd_in, fd_out, commands[cmd_i]);
+
+			// save the fd of the read pipe for the next process
+			fd_in = cur_pipe[0];
+
+			// close write end of pipe
+			//printf("closing %d in parent\n", cur_pipe[1]);
+			close(cur_pipe[1]);
+
+			// wait for child to terminate, if we're supposed to
+			if(!no_wait) {
+				//printf("waiting for child...\n");
+				waitpid(childPID, NULL, 0);
+			}
+
+		}
+
+		// redirect stdout to file, if necessary
+		if(outfiles[cmd_i] != NULL) {
+			// try to open the output file
+			fd_out = open(outfiles[cmd_i], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+			if(fd_out == -1) {
+				printf("open: Error: %s\n", strerror(errno));
+				fflush(stdout);
+				return 1;
+			}
+		} else {
+			fd_out = 1; // last child's output is parent's output by default
+		}
+		
+		// fork and exec
+		childPID = spawn_process(fd_in, fd_out, commands[cmd_i]);
+
+		// close write end of pipe
+		//printf("closing %d in parent\n", cur_pipe[1]);
+		close(cur_pipe[1]);
+
+		// wait for child to terminate, if we're supposed to
+		if(!no_wait) {
+			//printf("waiting for child...\n");
+			waitpid(childPID, NULL, 0);
+		}
+
 
 	}
 	return 0;
 }
 
+/*
+ * fork, redirect i/o, and exec command
+ */
+int spawn_process(int fd_in, int fd_out, char** cmd) {
+
+	//printf("spawning process: %s\n", cmd[0]);
+	// fork
+	pid_t childPID = fork();
+	if(childPID == 0) { // child process
+		if(fd_in != 0) {
+			//printf("redirecting stdin to %d\n", fd_in);
+			dup2(fd_in, 0);
+			//printf("closing %d in child\n", fd_in);
+			close(fd_in);
+		}
+		if(fd_out != 1) {
+			//printf("redirecting stdout to %d\n", fd_out);
+			dup2(fd_out, 1);
+			//printf("closing %d in child\n", fd_out);
+			close(fd_out);
+		}
+		return execvp(cmd[0], cmd);
+	} else { // parent process
+		return childPID;
+	}
+
+}
+
 /* 
-  *reads a single line from terminal
+ *reads a single line from terminal
  */
 char* readLine() {
 
@@ -221,6 +277,7 @@ char* readLine() {
 	char* line = (char*) malloc( MAX_TOKEN_LENGTH+1 );
 	if(getline(&line, &MAX_TOKEN_LENGTH, stdin) > MAX_TOKEN_LENGTH) {
 		printf("WARNING: line is longer than %d characters!\n", (int)MAX_TOKEN_LENGTH);
+		fflush(stdout);
 		return NULL;
 	}
 
@@ -228,7 +285,7 @@ char* readLine() {
 }
 
 /* 
-  *lex the line read into an array of tokens
+ *lex the line read into an array of tokens
  */
 char** lex(char* line) {
 
@@ -250,6 +307,7 @@ char** lex(char* line) {
 	// check if we quit because of going over allowed word limit
 	if (i == MAX_NUM_TOKENS) {
 		printf( "WARNING: line contains more than %d tokens!\n", (int)MAX_NUM_TOKENS ); 
+		fflush(stdout);
 	} 
 	else
 		tokens[i] = NULL;
@@ -258,3 +316,13 @@ char** lex(char* line) {
 	return tokens;
 }
 
+void print_strings(char** strings) {
+	int k=0;
+	while(strings[k] != NULL) {
+		printf("%s ", strings[k]);
+		fflush(stdout);
+		k++;
+	}
+	printf("\n");
+	fflush(stdout);
+}
